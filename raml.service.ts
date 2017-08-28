473ccfaaca3369235c2347d3ad0dbfbce266dd94
chinmaynpande <chinmay.pande@yahoo.com>
2017-08-29 00:42:02 +0530

    Add files via upload

---
+++ raml.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http , Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+
+
+@Injectable()
+export class  RamlService{
+
+constructor (
+
+private http :Http
+){}
+
+public    getRamlData(){
+
+return this.http.get('https://jsonplaceholder.typicode.com/posts').map((res:Response) => res);
+}
+}
